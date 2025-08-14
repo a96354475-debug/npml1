1e0ff169ea8789a9b2f594437da58396d16fdaf2
a96354475-debug <a96354475@gmail.com>
2025-08-14 14:43:49 +0900

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -9,13 +9,14 @@ import {
 
 // ⬇⬇ 이 부분에 콘솔에서 보여준 config를 그대로 붙여넣어
 export const firebaseConfig = {
-  apiKey: "AIzaSyA6p4h89vtAlho4eztwStPaYN0ORc6i0Q",
+  apiKey: "AIzaSyAS6p4h09vtAlho4eztwStPaYNO0rc6iOQ",
   authDomain: "npmlsite.firebaseapp.com",
   projectId: "npmlsite",
-  storageBucket: "npmlsite.appspot.com",
+  storageBucket: "npmlsite.firebasestorage.com",
   messagingSenderId: "186697073652",
   appId: "1:186697073652:web:95a7d17c736a0d95ffafa1"
 };
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
